135942a4fcdd30d93d4819c2a2566a0fb278565b
hasan <hasanhosseininum7@gmail.com>
2023-12-17 22:59:01 +0330

    add new column to migration files

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('phone_number');
+            $table->string('prefix_code')->default('98');
             $table->char('national_code' , 10)->nullable();
             $table->string('full_name')->nullable();
             $table->string('email')->nullable();
